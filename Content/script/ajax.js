904aa95401ee4f7be5afdf640145b1304308eb8c
Mighty599 <1518318962@qq.com>
2024-04-26 13:52:26 +0800

    更改student.html ajax.js

--- Content/script/ajax.js
+++ Content/script/ajax.js
@@ -1,15 +1,17 @@
-﻿// 获取页面中的图像标签
-const image = document.getElementById("my_image");
-
-// 定义图片数组和当前显示的图片索引
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-let currentIndex = 0;
-
-// 定义定时器，每5秒钟切换一张图片
-setInterval(() => {
-  // 切换到下一张图片
-  currentIndex = (currentIndex + 1) % images.length;
-  
-  // 更新图像标签的src属性以显示新图片
-  image.src = images[currentIndex];
-}, 5000);
+﻿function ajax({ url, type, data }) {
+	let user = getCurrentUser();
+	data.userid = user?.id;
+    return new Promise((res, rej) => {
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            success: function (data) {
+                res(data);
+            },
+            error: function (err) {
+                rej(err);
+            }
+        });
+    });
+}
